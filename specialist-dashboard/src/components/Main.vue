44d308594deddde71050c5ff1130dfc7715bb56b
Ohad Ron <ohad@ohadron.com>
2018-01-08 10:08:26 +0200

    New ticket form - first steps

--- specialist-dashboard/src/components/Main.vue
+++ specialist-dashboard/src/components/Main.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
+
     <div class="card-deck">
       <counter-display counterValue="23" description="פניות ממתינות" color="#F1F864"/>
       <counter-display counterValue="12" description="פניות בביצוע" color="#BDE4F4"/>
       <counter-display counterValue="3" description="פניות בטיוטה" color="#FAFCDB"/>
       <counter-display counterValue="7" description="פניות שטרם תואמו" color="#FFD8D8"/>
     </div>
+    <router-link to="/new-ticket" tag="button" type="button" class="btn btn-primary btn-lg m-4 mx-auto d-block w-25">פניה חדשה</router-link>
     <ticket-table />
   </div>
 </template>
