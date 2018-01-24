f4d28bad4c5c1aa7948dd0f06b53a63243420287
Ohad Ron <inverted3@gmail.com>
2018-01-24 10:49:24 +0200

    Added vue-apollo

--- specialist-dashboard/src/main.js
+++ specialist-dashboard/src/main.js
@@ -4,14 +4,37 @@ require('../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss')
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import 'font-awesome/css/font-awesome.css';
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import VueApollo from 'vue-apollo'
+import 'font-awesome/css/font-awesome.css'
 
 Vue.config.productionTip = false
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:5000/graphql',
+})
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+})
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  apolloProvider,
   template: '<App/>',
   components: { App }
 })
+
+
