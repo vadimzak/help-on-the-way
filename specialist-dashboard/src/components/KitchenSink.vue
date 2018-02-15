0bbaeb3514693a41b60fe740880959d67a233250
Ohad Ron <ohad@ohadron.com>
2018-02-15 22:46:11 +0200

    Group message preview component (#60)

--- specialist-dashboard/src/components/KitchenSink.vue
+++ specialist-dashboard/src/components/KitchenSink.vue
@@ -22,18 +22,30 @@
     <h4>בוחר כתובת</h4>
     <address-picker/>
 
+    <h4>תצוגה מקדימה להודעה קבוצתית</h4>
+    <group-message-preview 
+      :ticket="exampleTicket"
+    />
+
   </div>
 </template>
 
 <script>
 import RadioBoxes from '@/components/RadioBoxes'
 import AddressPicker from '@/components/AddressPicker'
+import GroupMessagePreview from '@/components/GroupMessagePreview'
 
 export default {
-  components: { RadioBoxes, AddressPicker },
+  components: { RadioBoxes, AddressPicker, GroupMessagePreview },
   data () {
     return {
-      
+      exampleTicket:{
+        elderFirstName: "שמוליק",
+        elderAddress: "המייסדים 25, גבעתיים",
+        ticketDescription: "ליווי לקניית רדיאטור חדש",
+        ticketTiming: "היום או מחר",
+        ticketPermalink: "https://bit.ly/dorldor"
+      }
     }
   }
 }
