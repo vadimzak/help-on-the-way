d805c86c2655bc5f91a486632a60749f27ff6433
barakd <barak.drechsler@gmail.com>
2018-02-17 11:44:23 +0200

    align migration between heorku & docker-compose setup

--- server/migrations/migrate.js
+++ server/migrations/migrate.js
@@ -1,5 +1,9 @@
-const { PGHOST, PGPORT, PGDATABASE, DB_SCHEMA, PGUSER, PGPASSWORD, PROD_ENV, DATABASE_URL } = process.env;
-if (PROD_ENV == 'true') {
+const { PGHOST, PGPORT, PGDATABASE, DB_SCHEMA, PGUSER, PGPASSWORD, PROD_ENV, DATABASE_URL, DO_MIGRATIONS } = process.env;
+if (DO_MIGRATIONS == 'true') {
+    const connectionString = DATABASE_URL || `postgresql://${PGHOST}:5432/${PGDATABASE}`
+    console.log('executing migrations...');
     require('child_process')
-    .execSync(`flyway -url="jdbc:${DATABASE_URL}" -user=${PGUSER} -password=${PGPASSWORD} -locations=filesystem:./migrations/sql migrate`, { stdio: [0, 1, 2] });
-}  
+    .execSync(`flyway -url="jdbc:${connectionString}" -schemas=${DB_SCHEMA} -user=${PGUSER} -password=${PGPASSWORD} -locations=filesystem:./migrations/sql migrate`, { stdio: [0, 1, 2] });
+} else {
+    console.log('No DO_MIGRATIONS flag, skiping migrations...');
+}
--- server/migrations/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/wait-for-postgres.sh flyway -url=jdbc:postgresql://db:5432/${PGDATABASE} -schemas=${DB_SCHEMA} -user=${PGUSER} -password=${PGPASSWORD} migrate
