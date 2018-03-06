8e5962f14d9151f7bc160ec309cc0831f14dd6c9
barakd <barak.drechsler@gmail.com>
2018-03-06 19:38:14 +0200

    adding the missing graphql client

---
+++ volunteer/src/graphql/queries/ticket.js
@@ -0,0 +1,37 @@
+import gql from 'graphql-tag'
+
+export const GET_BY_ID = gql`
+mutation getTicketById($ticketId: BigInt!){
+    getTicketById(input: {
+      ticketId: $ticketId
+    }) {
+      ticketForVolunteer{
+  ticketId,
+  description,
+  durationEta,
+  issuingInstitue,
+  dueDate,
+  status,
+  maxVolunteers,
+  elderId,
+  elderFirstName,
+  elderLastName,
+  elderPhoneNumber,
+  startAddressId,
+  startAddressCity,
+  startAddressStreet,
+  startAddressHouseNumber,
+  startAddressEnteranced,
+  destinationAddressId,
+  destinationAddressCity,
+  destinationAddressStreet,
+  destinationAddressHouseNumber,
+  destinationAddressEnteranced,
+  endAddressId,
+  endAddressCity,
+  endAddressStreet,
+  endAddressHouseNumber,
+  endAddressEnteranced,
+      }
+    }
+  }`
