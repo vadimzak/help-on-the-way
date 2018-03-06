67bf8e9a0f67edabcde8dd9df8893c5e2b1dfac3
barakd <barak.drechsler@gmail.com>
2018-03-06 23:53:13 +0200

    add missing sub categories

---
+++ server/migrations/sql/V61__add_ticket_sub_categories.sql
@@ -0,0 +1,6 @@
+ALTER TYPE help.ticket_sub_category_type ADD VALUE 'repair_electricity';
+ALTER TYPE help.ticket_sub_category_type ADD VALUE 'repair_multimedia';
+ALTER TYPE help.ticket_sub_category_type ADD VALUE 'repair_infrastructure';
+ALTER TYPE help.ticket_sub_category_type ADD VALUE 'repair_furniture';
+ALTER TYPE help.ticket_sub_category_type ADD VALUE 'repair_doors_windows';
+ALTER TYPE help.ticket_category_type ADD VALUE 'social_at_home';
