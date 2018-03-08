c4352e5d96754c55392d258d3d925d41181ba5c0
barakd <barak.drechsler@gmail.com>
2018-03-08 09:20:43 +0200

    search rename

--- specialist-dashboard/src/components/features/elder/index.js
+++ specialist-dashboard/src/components/features/elder/index.js
@@ -1,2 +1,5 @@
-import ElderSearchComp from './elderSearch/ElderSearch'
-export const ElderSearch = ElderSearchComp;
+import search from './search/ElderSearch'
+import elderForm from './create/ElderForm'
+
+export const ElderSearch = search;
+export const ElderForm = elderForm;
