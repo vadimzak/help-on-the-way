d3883cfba1166cf7ae084df41bc4ba0da306d63d
barakd <barak.drechsler@gmail.com>
2018-03-08 09:18:51 +0200

    adding elder form

---
+++ specialist-dashboard/src/components/features/elder/create/ElderForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <b-form  @submit="onSubmit">
+      <b-form-group>
+              <label for="elderFirstName">שם פרטי</label>
+              <b-form-input id="elderFirstName" type="text" v-model="elder.firstName" required></b-form-input>
+              <label for="elderLastName">שם משפחה </label>
+              <b-form-input id="elderLastName" type="text" v-model="elder.lastName" required></b-form-input>
+      </b-form-group>
+      <b-form-group>
+              <label for="elderPhone">טלפון</label>
+              <b-form-input id="elderPhone" type="text" v-model="elder.phone" required></b-form-input>
+      </b-form-group>
+    <b-form-group label="מין">
+      <b-form-radio-group id="elderGender" v-model="elder.gender" name="gender" required>
+        <b-form-radio value="male">זכר</b-form-radio>
+        <b-form-radio value="female">נקבה</b-form-radio>
+      </b-form-radio-group>
+    </b-form-group>
+    <b-form-group label="כתובת">
+        <InputAddress :keepOpen="true" :address="elder.address" :locationChanged="(address) => elder.address = address"></InputAddress>
+    </b-form-group>    
+    <b-form-group label="תאריך לידה">
+        <DatePicker language="he" required></DatePicker>
+    </b-form-group>
+    <b-form-row>
+        <b-button type="submit" variant="secondary">
+            שמור והמשך
+        </b-button>
+    </b-form-row>
+  </b-form>
+
+</template>
+<script>
+import DatePicker from 'vuejs-datepicker'
+import InputAddress from '@/components/InputAddress'
+
+export default {
+  props: ['elder', 'onSave'],
+  components: { DatePicker, InputAddress },
+  methods: {
+      onSubmit(event){
+        event.preventDefault();
+        this.onSave(this.elder);
+      }
+  }
+}
+</script>
+<style>
+
+</style>
