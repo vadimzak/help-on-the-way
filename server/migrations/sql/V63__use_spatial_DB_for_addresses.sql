43bc115f85080689ae4a57e02b0602c565c9b111
Barak Drechsler <barak.drechsler@gmail.com>
2018-03-10 15:51:03 +0200

    Update V63__use_spatial_DB_for_addresses.sql

--- server/migrations/sql/V63__use_spatial_DB_for_addresses.sql
+++ server/migrations/sql/V63__use_spatial_DB_for_addresses.sql
@@ -9,11 +9,11 @@ drop column lat;
 
 comment on column help.address.geom is 'the raw location data, expects a postgis point, as SRID=4326;POINT(25800 256000) long lat, will return hex value of the location, use coordinates to get json of the points.';
 
-create function help.address_coordinates(a help.address)
+create function help.address_location(a help.address)
 returns jsonb as $$
   select ST_AsGeoJSON(a.geom)::jsonb
 $$ language sql stable;
 
-comment on function help.address_coordinates (a help.address) is 'a coumputed column, that returns the geom field value, as a readable points json';
+comment on function help.address_location (a help.address) is 'a coumputed column, that returns the geom field value, as a readable points json';
 
-GRANT EXECUTE ON FUNCTION help.address_coordinates(a help.address) TO "SPECIALIST";
+GRANT EXECUTE ON FUNCTION help.address_location(a help.address) TO "SPECIALIST";
