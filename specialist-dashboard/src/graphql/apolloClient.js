e570cf61f3ae629deee4d2a81e85dfd77e242cd6
Barak Drechsler <barak.drechsler@gmail.com>
2018-03-13 14:31:17 +0200

    Update apolloClient.js

--- specialist-dashboard/src/graphql/apolloClient.js
+++ specialist-dashboard/src/graphql/apolloClient.js
@@ -3,7 +3,6 @@ import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import VueApollo from 'vue-apollo'
 import Vue from 'vue'
-console.log(process.env);
 const link = new HttpLink({
     uri: process.env.SERVER_BASE_URL,
 });
