f1fc0ba18388942c86c7d34ba6e33e500bbf57aa
Admon Sasson <admon.s@walkme.com>
2018-03-18 00:31:13 +0200

    fixed me endpoint (#136)

--- server/src/endpoints/me.js
+++ server/src/endpoints/me.js
@@ -1,11 +1,24 @@
-const { PG_DEFAULT_ROLE } = process.env;
+const jsonwebtoken = require('jsonwebtoken');
+const { JWT_SECRET, PG_DEFAULT_ROLE } = process.env;
 const DEFAULT_ROLE = PG_DEFAULT_ROLE == 'postgres' ? 'ADMIN' : PG_DEFAULT_ROLE;
 
 const me = (req, res) => {
-	const { firstName, lastName, type } = req.user || { firstName: 'Anonymous', lastName: '', type: DEFAULT_ROLE };
+	const { firstName, lastName } = req.user || { firstName: 'Anonymous', lastName: '' };
 	const name = `${firstName} ${lastName}`.trim();
 
-	res.json({ name, role: type });
+	const { person_id, type } = extractAuthorizationFromHeader(req) || { person_id: 0, type: DEFAULT_ROLE };
+
+	res.json({ person_id, name, role: type });
+};
+
+const extractAuthorizationFromHeader = req => {
+	const auth = req.session.authorization;
+	if (!auth) return null;
+
+	const jwt = auth.replace(/Bearer /, '');
+
+	const { person_id, type } = jsonwebtoken.verify(jwt, JWT_SECRET);
+	return { person_id, type };
 };
 
 module.exports = me;
