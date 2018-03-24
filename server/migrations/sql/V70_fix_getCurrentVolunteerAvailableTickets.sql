f86f3e05671af3762ffc323374b03e534ad7f217
barakd <barak.drechsler@gmail.com>
2018-03-24 15:16:50 +0300

    fix getCurrentVolunteerAvilableTickets to return set of results

---
+++ server/migrations/sql/V70_fix_getCurrentVolunteerAvailableTickets.sql
@@ -0,0 +1,17 @@
+drop function help."getCurrentVolunteerAvailableTickets"();
+
+create or replace function help."getCurrentVolunteerAvailableTickets"() returns setof help.ticket_for_volunteer as $$
+select t.* 
+from 
+	help.ticket_for_volunteer t
+	inner join help.ticket_groups tg on tg.ticket_id = t.id
+	inner join help.group g on g.id = tg.group_id
+	inner join help.group_volunteers gv on tg.group_id = g.id
+	inner join help.person p on p.id = gv.volunteer_id
+where
+	p.id = help_private.getCurrentUserId()
+	and t.status = 'open'
+$$ language sql stable security definer;
+
+REVOKE ALL ON FUNCTION help."getCurrentVolunteerAvailableTickets"() FROM PUBLIC;
+GRANT EXECUTE ON FUNCTION help."getCurrentVolunteerAvailableTickets"() TO "VOLUNTEER";
