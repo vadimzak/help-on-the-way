49efbeef1da2034d9d9a9f3d8d8e8b76d8dd6c5e
barakd <barak.drechsler@gmail.com>
2018-04-02 20:20:45 +0300

    adding elder mobility

---
+++ shared/constants/enums/ElderMobilityType.js
@@ -0,0 +1,22 @@
+export default {
+    INDEPENDENT: {
+        icon: 'accessibility',
+        text: 'ללא סיוע',
+        value: 'INDEPENDENT',
+    },
+    WALKING_STICK: {
+        icon: 'rowing',
+        text: 'מקל/הליכון',
+        value: 'WALKING_STICK',
+    },
+    WHEEL_CHAIR: {
+        icon: 'accessible',
+        text: 'כיסא גלגלים',
+        value: 'WHEEL_CHAIR',
+    },
+    NEEDS_SUPPORT: {
+        icon: 'supervisor_account',
+        text: 'תמיכה וסיוע',
+        value: 'NEEDS_SUPPORT',
+    },
+}
