39e5db33755ca47128b37523493cf3745792bb7d
barakd <barak.drechsler@gmail.com>
2018-04-02 20:05:45 +0300

    adding ticket transport types enums

---
+++ shared/constants/enums/TicketTransportType.js
@@ -0,0 +1,38 @@
+export default {
+    WALK: {
+        icon: '',
+        text: 'הליכה',
+        value: 'WALK',
+    },
+
+    BUS: {
+        icon: '',
+        text: 'אוטובוס',
+        value: 'WALK',
+    },
+    TAXI: {
+        icon: '',
+        text: 'מונית',
+        value: 'TAXI',
+    },
+    TRAIN: {
+        icon: '',
+        text: 'רכבת',
+        value: 'TRAIN',
+    },
+    PRIVATE_CAR_ELDER: {
+        icon: '',
+        text: 'מכונית',
+        value: 'PRIVATE_CAR_ELDER',
+    },
+    PRIVATE_CAR_VOLUNTEER: {
+        icon: '',
+        text: 'מכונית',
+        value: 'PRIVATE_CAR_VOLUNTEER',
+    },
+    PRIVATE_CAR_OTHER: {
+        icon: '',
+        text: 'מכונית',
+        value: 'PRIVATE_CAR_OTHER',
+    },
+}
