a0a7fae1a58beb26257825698f55272601e33ae9
barakd <barak.drechsler@gmail.com>
2018-04-05 01:10:46 +0300

    test deploy [deploy-volunteer]

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,16 +1,20 @@
 cd ./deployment && npm i && cd ..
 eval $(node ./deployment/should-deploy.js)
-export DEPLOY_PROD=true
-echo should deploy: volunteer: $DEPLOY_VOLUNTEER, dashboard: $DEPLOY_DASHBOARD, server: $DEPLOY_SERVER
+echo should deploy: volunteer: $DEPLOY_VOLUNTEER, dashboard: $DEPLOY_DASHBOARD, server: $DEPLOY_SERVER to $DEPLOY_TO
+if [ $DEPLOY_TO = "STAGE" ]
+then
+export SERVER_BASE_URL="http://server.dev.dorldor.org.il"
+export APP_URL="http://app.dev.dorldor.org.il"
+fi
 if [ $DEPLOY_VOLUNTEER = true ]
 then
 echo "building volunteer app"
-#cd ./volunteer && npm i && npm run build
-#cd ..
+cd ./volunteer && npm i && npm run build
+cd ..
 fi
 if [ $DEPLOY_DASHBOARD = true ]
 then
 echo "building dashboard app"
-#cd ./specialist-dashboard && npm i && npm run build
-#cd ..
+cd ./specialist-dashboard && npm i && npm run build
+cd ..
 fi
