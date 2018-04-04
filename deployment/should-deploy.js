fd67fce10aee6d323b2bdcacd5132b99b6dac942
barakd <barak.drechsler@gmail.com>
2018-04-05 01:02:29 +0300

    test [deploy-dashboard]

--- deployment/should-deploy.js
+++ deployment/should-deploy.js
@@ -2,12 +2,13 @@ const git = require('simple-git/promise')();
 const diffSummary = require('simple-git/');
 const commitId = process.env.TRAVIS_COMMIT;
 async function deploy() {
+    const DEPLOY_ENV = 'PROD';
     const commit = await git.show([commitId, '-s', '--format=%B']);
     const diff = await git.diffSummary([commitId + '^', commitId]);
     const shouldDeployServer = commitContains(commit, '[deploy-server]') || containsFileFrom(diff.files, 'server/');
     const shouldDeployDashboard = commitContains(commit, '[deploy-dashboard]') || containsFileFrom(diff.files, 'specialist-dashboard/');
     const shouldDeployVolunteerApp = commitContains(commit, '[deploy-volunteer]') || containsFileFrom(diff.files, 'volunteer/');
-    console.log(`export DEPLOY_SERVER=${shouldDeployServer} && export DEPLOY_VOLUNTEER=${shouldDeployVolunteerApp} && export DEPLOY_DASHBOARD=${shouldDeployDashboard}`);
+    console.log(`export DEPLOY_SERVER=${shouldDeployServer} && export DEPLOY_VOLUNTEER=${shouldDeployVolunteerApp} && export DEPLOY_DASHBOARD=${shouldDeployDashboard} && export DEPLOY_TO=${DEPLOY_ENV}`);
 }
 
 function containsFileFrom(files, from) {
