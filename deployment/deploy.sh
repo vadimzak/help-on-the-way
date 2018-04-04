81b2f8970f359b427e97058dd9ff45d39f4ca9d4
barakd <barak.drechsler@gmail.com>
2018-04-05 00:57:20 +0300

    [deploy-dashboard] test again

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -5,12 +5,12 @@ echo should deploy: volunteer: $DEPLOY_VOLUNTEER, dashboard: $DEPLOY_DASHBOARD, 
 if [ $DEPLOY_VOLUNTEER = true ]
 then
 echo "building volunteer app"
-cd ./volunteer && npm i && npm run build
-cd ..
+#cd ./volunteer && npm i && npm run build
+#cd ..
 fi
 if [ $DEPLOY_DASHBOARD = true ]
 then
 echo "building dashboard app"
-cd ./specialist-dashboard && npm i && npm run build
-cd ..
+#cd ./specialist-dashboard && npm i && npm run build
+#cd ..
 fi
