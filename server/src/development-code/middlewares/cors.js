61b9ba081c98e9059ec00a620e498fe3c1214241
barakd <barak.drechsler@gmail.com>
2018-04-05 13:53:35 +0300

    whitlist as array

--- server/src/development-code/middlewares/cors.js
+++ server/src/development-code/middlewares/cors.js
@@ -1,13 +1,7 @@
 const cors = require('cors');
 const whiteList = process.env.CORS_FOR_DEV_VOLUNTEER_APP_URL ? process.env.CORS_FOR_DEV_VOLUNTEER_APP_URL.split(',') : [];
 const corsOptions = {
-	origin(origin, callback) {
-		if (whiteList.includes(origin)) {
-			callback(null, true)
-		  } else {
-			callback(new Error('Not allowed by CORS'))
-		  }
-	},
+	origin: whiteList,
 	credentials: true
 };
 
