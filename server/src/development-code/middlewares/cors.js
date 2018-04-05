8cc33c63406973ea6f8b1d4ae6e9f4ff22b0b314
barakd <barak.drechsler@gmail.com>
2018-04-05 13:35:12 +0300

    fix whitelist

--- server/src/development-code/middlewares/cors.js
+++ server/src/development-code/middlewares/cors.js
@@ -2,7 +2,7 @@ const cors = require('cors');
 const whiteList = process.env.CORS_FOR_DEV_VOLUNTEER_APP_URL ? process.env.CORS_FOR_DEV_VOLUNTEER_APP_URL.split(',') : [];
 const corsOptions = {
 	origin(origin, callback) {
-		if (whitelist.includes(origin)) {
+		if (whiteList.includes(origin)) {
 			callback(null, true)
 		  } else {
 			callback(new Error('Not allowed by CORS'))
