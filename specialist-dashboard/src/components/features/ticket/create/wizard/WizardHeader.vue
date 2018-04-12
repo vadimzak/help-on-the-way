80df16e867088274cf6de9facc118ef0a011be0a
Assaf Shilo <assaf317@gmail.com>
2018-04-12 20:29:25 +0300

    Fix css and replace h2 to h3 (#187)

--- specialist-dashboard/src/components/features/ticket/create/wizard/WizardHeader.vue
+++ specialist-dashboard/src/components/features/ticket/create/wizard/WizardHeader.vue
@@ -1,24 +1,36 @@
 <template>
   <div class="my-4 d-flex align-items-start steps-bar">
         <div class="p-3 d-flex flex-column  align-items-start justify-content-start">
-            <Truncate class="h2" v-if="ticket.elder" ><span @click="() => $store.commit('createTicket/setStep', 1)">{{ticket.elder | formatName}}</span></Truncate>
-            <Truncate class="" v-if="ticket.elder">{{ticket.elder.address | formatAddress}}</Truncate>
+            <Truncate class="h3" v-if="ticket.elder" >
+                <span @click="() => $store.commit('createTicket/setStep', 1)">{{ticket.elder | formatName}}</span>
+            </Truncate>
+            <Truncate class="" v-if="ticket.elder">
+                {{ticket.elder.address | formatAddress}}
+            </Truncate>
         </div>
         <div class="p-3 d-flex flex-column  align-items-start justify-content-start ">
-            <Truncate class="h2" v-if="ticket.category"><span  @click="() => $store.commit('createTicket/setStep', 2)">{{ticket.category| formatCategory}}</span></Truncate>
-            <Truncate class="" v-if="ticket.destinationAddress">{{ticket.destinationAddress | formatAddress}}</Truncate>
+            <Truncate class="h3" v-if="ticket.category">
+                <span  @click="() => $store.commit('createTicket/setStep', 2)">{{ticket.category| formatCategory}}</span>
+            </Truncate>
+            <Truncate class="" v-if="ticket.destinationAddress">
+                {{ticket.destinationAddress | formatAddress}}
+            </Truncate>
             <Truncate v-if="!ticket.destinationAddress">
                 &nbsp;                
             </Truncate>
         </div>
         <div class="p-3 d-flex flex-column  align-items-start justify-content-center ">
-            <Truncate class="h2" v-if="ticket.durationEta"><span @click="() => $store.commit('createTicket/setStep', 3)">{{ticket.durationEta| formatMinutes}}</span></Truncate>
-            <Truncate>
+            <Truncate class="h3" v-if="ticket.durationEta">
+                <span @click="() => $store.commit('createTicket/setStep', 3)">{{ticket.durationEta| formatMinutes}}</span>
+            </Truncate>
+            <Truncate v-if="ticket.durationEta">
                 שעות הבוקר
             </Truncate>        
         </div>
         <div class="p-3 d-flex flex-column  align-items-start justify-content-center ">
-                <Truncate class="h2" v-if="ticket.groups && ticket.groups.length"><span @click="() => $store.commit('createTicket/setStep', 5)">{{ticket.groups[0].channels}}</span></Truncate>
+            <Truncate class="h3" v-if="ticket.groups && ticket.groups.length">
+                <span @click="() => $store.commit('createTicket/setStep', 5)">{{ticket.groups[0].channels}}</span>
+            </Truncate>
             <Truncate>                
                 &nbsp;
             </Truncate>   
@@ -61,19 +73,20 @@ export default {
 </script>
 <style scoped>
     .steps-bar{
-        margin: -0.75rem;
+        margin: -0.5rem;
     }
     .steps-bar > div{
         background-color: var(--white);
         border-bottom: 7px solid var(--blue);
         box-shadow: 0px 2px 8px 0 rgba(0, 0, 0, 0.08);
         flex: 1;
+        height: 95px;
         width: 50px;
     }
 
     .steps-bar > div:last-child{
         border: none;
-        height: 108px;
+        height: 95px;
     }
 
 </style>
